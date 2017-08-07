5a20f97055271cfde9213eccb26b161c61d50012
Aleksander Naumenok <heaven_knight@hotmail.com>
2017-08-07 13:28:55 +0200

    Project start, first commit.

---
+++ CLR_Calculator/Main.cpp
@@ -0,0 +1,15 @@
+#include "MainWindow.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	CLR_Calculator::MainWindow window;
+	Application::Run(%window);
+}
